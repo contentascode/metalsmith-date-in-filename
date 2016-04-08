593682d6808e27e6866cab9efa36c05cb8efc78e
Jun Matsushita <jun@iilab.org>
2016-04-08 19:52:14 +0200

    Adding basename option to provide dateless slug

--- index.js
+++ index.js
@@ -10,12 +10,14 @@ module.exports = function(options){
       var m;
       if (m = fileName.match(/(\d{4}-\d{2}-\d{2})/)) {
           fileMeta.date = new Date(m[1]);
+          if (options.basename) fileMeta[options.basename] = fileMeta.basename.replace(/(\d{4}-\d{2}-\d{2})/g, '').replace(/^[^a-z\d]*|[^a-z\d]*$/gi, '')
       } else if (m = fileName.match(/(\d{8})/)) {
           fileMeta.date = new Date(
               m[1].substr(0, 4) +'-'+
               m[1].substr(4, 2) +'-'+
               m[1].substr(6, 2)
           );
+          if (options.basename) fileMeta[options.basename] = fileMeta.basename.replace(/(\d{8})/g, '').replace(/^[^a-z\d]*|[^a-z\d]*$/gi, '')
       }
     });
     var filesWithoutContents = _.zipObject(_.map(files, function (fileMeta, fileName) {
